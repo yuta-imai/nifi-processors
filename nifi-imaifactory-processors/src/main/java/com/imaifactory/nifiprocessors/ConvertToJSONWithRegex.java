28d0cfc700122be11dfae86f60a5c4520a787558
Yuta Imai <imai.factory@gmail.com>
2016-05-08 16:00:54 +0900

    Adding nifi-imaifactory-processors/src/main/java/com/imaifactory/nifiprocessors/ConvertToJSONWithRegex.java

--- nifi-imaifactory-processors/src/main/java/com/imaifactory/nifiprocessors/ConvertToJSONWithRegex.java
+++ nifi-imaifactory-processors/src/main/java/com/imaifactory/nifiprocessors/ConvertToJSONWithRegex.java
@@ -16,15 +16,16 @@
  */
 package com.imaifactory.nifiprocessors;
 
-import am.ik.ltsv4j.LTSV;
-import am.ik.ltsv4j.LTSVParser;
+import org.apache.nifi.annotation.behavior.*;
 import org.apache.nifi.annotation.documentation.CapabilityDescription;
+import org.apache.nifi.annotation.documentation.Tags;
 import org.apache.nifi.components.PropertyDescriptor;
 import org.apache.nifi.flowfile.FlowFile;
 import org.apache.nifi.flowfile.attributes.CoreAttributes;
 import org.apache.nifi.processor.*;
 import org.apache.nifi.processor.exception.ProcessException;
 import org.apache.nifi.processor.io.StreamCallback;
+import org.apache.nifi.processor.util.StandardValidators;
 import org.apache.nifi.stream.io.BufferedOutputStream;
 import org.codehaus.jackson.map.ObjectMapper;
 
@@ -32,29 +33,47 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
-@CapabilityDescription("The processor which converts LTSV to JSON")
-public class ConvertLTSVToJSON extends AbstractProcessor {
+@EventDriven
+@SideEffectFree
+@SupportsBatching
+@Tags({"json", "attributes", "flowfile"})
+@InputRequirement(InputRequirement.Requirement.INPUT_REQUIRED)
+@WritesAttribute(attribute = "JSONAttributes", description = "JSON representation of Attributes")
+@CapabilityDescription("The processor which converts text data to JSON with Regex")
+public class ConvertToJSONWithRegex extends AbstractProcessor {
 
     private static final String APPLICATION_JSON = "application/json";
 
     public static final Relationship REL_SUCCESS = new Relationship.Builder().name("success")
             .description("Successfully converted attributes to JSON").build();
+
     public static final Relationship REL_FAILURE = new Relationship.Builder().name("failure")
             .description("Failed to convert attributes to JSON").build();
 
-    private List<PropertyDescriptor> descriptors;
+    public static final PropertyDescriptor REGEX = new PropertyDescriptor.Builder()
+            .name("Regular Expression")
+            .required(false)
+            .description("Regular expression with named keys to extract values from text")
+            .addValidator(StandardValidators.NON_EMPTY_VALIDATOR)
+            .build();
+
+    private List<PropertyDescriptor> properties;
 
     private Set<Relationship> relationships;
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
-    private static final LTSVParser ltsvParser = LTSV.parser();
 
     @Override
     protected void init(final ProcessorInitializationContext context) {
-        final List<PropertyDescriptor> descriptors = new ArrayList<PropertyDescriptor>();
-        this.descriptors = Collections.unmodifiableList(descriptors);
+
+        final List<PropertyDescriptor> properties = new ArrayList<>();
+        properties.add(REGEX);
+        this.properties = Collections.unmodifiableList(properties);
+
         final Set<Relationship> relationships = new HashSet<Relationship>();
         relationships.add(REL_SUCCESS);
         relationships.add(REL_FAILURE);
@@ -62,13 +81,13 @@ public class ConvertLTSVToJSON extends AbstractProcessor {
     }
 
     @Override
-    public Set<Relationship> getRelationships() {
-        return this.relationships;
+    protected List<PropertyDescriptor> getSupportedPropertyDescriptors() {
+        return properties;
     }
 
     @Override
-    public final List<PropertyDescriptor> getSupportedPropertyDescriptors() {
-        return descriptors;
+    public Set<Relationship> getRelationships() {
+        return relationships;
     }
 
     @Override
@@ -77,13 +96,24 @@ public class ConvertLTSVToJSON extends AbstractProcessor {
         if ( original == null ) {
             return;
         }
+        final String regex = context.getProperty(REGEX).toString();
+        //TODO Should be moved to initialization phase.
+        final Set keys = getNamedGroupCandidates(regex);
+        //TODO Should validate that regex includes keys.
+        final Pattern pattern = Pattern.compile(regex);
 
         try {
             FlowFile result = session.write(original, new StreamCallback() {
                 @Override
                 public void process(InputStream in, OutputStream out) throws IOException {
                     try (OutputStream outputStream = new BufferedOutputStream(out)) {
-                        outputStream.write(objectMapper.writeValueAsBytes(ltsvParser.parseLines(in)));
+                        String line = in.toString();
+                        Matcher matches = pattern.matcher(line);
+                        if(matches.find()) {
+                            Map parsed = parse(keys, matches);
+                        }else {
+                            return;
+                        }
                     }
                 }
             });
@@ -93,5 +123,24 @@ public class ConvertLTSVToJSON extends AbstractProcessor {
             getLogger().error(e.getMessage());
             session.transfer(original, REL_FAILURE);
         }
+    }
+
+    private static Set<String> getNamedGroupCandidates(String regex) {
+        Set<String> namedGroups = new TreeSet<String>();
+
+        Matcher m = Pattern.compile("\\(\\?<([a-zA-Z][a-zA-Z0-9]*)>").matcher(regex);
+
+        while (m.find()) {
+            namedGroups.add(m.group(1));
+        }
+        return namedGroups;
+    }
+
+    private static Map parse(Set keys, Matcher matches) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        for(Object key: keys){
+            map.put(key.toString(), matches.group(key.toString()));
+        }
+        return map;
     }
 }
---
+++ nifi-imaifactory-processors/src/test/java/com/imaifactory/nifiprocessors/ConvertToJSONWithRegexTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.imaifactory.nifiprocessors;
+
+import org.apache.nifi.flowfile.FlowFile;
+import org.apache.nifi.processor.ProcessSession;
+import org.apache.nifi.processor.io.StreamCallback;
+import org.apache.nifi.stream.io.BufferedOutputStream;
+import org.apache.nifi.util.TestRunner;
+import org.apache.nifi.util.TestRunners;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class ConvertToJSONWithRegexTest {
+
+    private TestRunner testRunner;
+
+    @Before
+    public void init() {
+        testRunner = TestRunners.newTestRunner(ConvertToJSONWithRegex.class);
+    }
+
+    @Test
+    public void testProcessor1() {
+        testRunner.setProperty(ConvertToJSONWithRegex.REGEX, "\\?<word>test ?<number>[0-9]+");
+        ProcessSession session = testRunner.getProcessSessionFactory().createSession();
+
+        FlowFile ff = session.create();
+        ff = session.write(ff, new StreamCallback() {
+            @Override
+            public void process(InputStream in, OutputStream out) throws IOException {
+                try (OutputStream outputStream = new BufferedOutputStream(out)) {
+                    outputStream.write("atest123abc".getBytes());
+                }
+            }
+        });
+
+        testRunner.enqueue(ff);
+        testRunner.run();
+        testRunner.assertTransferCount(ConvertToJSONWithRegex.REL_SUCCESS, 1);
+        testRunner.assertTransferCount(ConvertToJSONWithRegex.REL_FAILURE, 0);
+        testRunner.getProcessor();
+
+    }
+
+}
